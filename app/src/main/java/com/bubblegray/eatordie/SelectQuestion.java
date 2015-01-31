32de958831497371fff7c9e9f68455a01c169a64
aj211y <aj211y@users.noreply.github.com>
2015-01-31 15:27:34 +0800

    Update SelectQuestion.java

--- app/src/main/java/com/bubblegray/eatordie/SelectQuestion.java
+++ app/src/main/java/com/bubblegray/eatordie/SelectQuestion.java
@@ -36,4 +36,39 @@ public class SelectQuestion extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    
+    static String[] foodType={
+		"start","正餐","小點心","餓死了","有點飽","下午茶","宵夜"
+		,"甜的","鹹的","米飯","麵食","餃類","關東煮"
+		,"重鹹","清淡","中式","西式","日式","泰式"
+		,"冷的","熱的","平價","高檔","速食","定食"
+		,"肉類","蔬菜","牛肉","豬肉","雞肉","鴨肉","鵝肉","魚肉"
+	};
+	static Boolean visited[]={true};
+	static int RanNum()
+	{
+		Random rand = new Random();
+		return rand.nextInt(31)+1;
+	}
+	static void setup()
+	{
+	    Map food=new HashMap();
+		//build the id-food map
+		for(int i=1;i<foodType.length;i++)
+			food.put(i, foodType[i]);
+	}
+	public int TwoChooseOne(){
+		int id=0;
+		//random out a number that hasn't visit
+		while(id=RanNum()!=0 && !visited[id]){}
+		visited[id]=false;
+		if(id%2==1)
+		    visited[id+1]=true;
+		else
+		{
+		    visited[id-1]=true;
+		    id-=1;
+		}
+    	return id;
+	}
 }
