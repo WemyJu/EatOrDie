633fb593398f3fd91e84251725a7e2a851cdf0c0
WemyJu <wemyju@gmail.com>
2015-01-31 17:03:25 +0800

    java.class - SelectionQuestion still has some logic error

--- app/src/main/java/com/bubblegray/eatordie/SelectQuestion.java
+++ app/src/main/java/com/bubblegray/eatordie/SelectQuestion.java
@@ -11,14 +11,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -28,7 +33,17 @@ public class SelectQuestion extends ActionBarActivity {
     //View mContentView;
     View mLoadingView;
     int mLongDurationAnimation;
-    private TextView mTextView;
+    private TextView mTextView, choice1, choice2;
+    int i;
+    ArrayList<String> decisions = new ArrayList<String>();
+    static String[] foodType={
+            "start","正餐","小點心","餓死了","有點飽","下午茶","宵夜"
+            ,"甜的","鹹的","米飯","麵食","餃類","關東煮"
+            ,"重鹹","清淡","中式","西式","日式","泰式"
+            ,"冷的","熱的","平價","高檔","速食","定食"
+            ,"肉類","蔬菜","牛肉","豬肉","雞肉","鴨肉","鵝肉","魚肉"
+    };
+    static boolean visited[]= new boolean[40];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,35 +51,14 @@ public class SelectQuestion extends ActionBarActivity {
         setContentView(R.layout.activity_select_question);
 
         mTextView = (TextView) findViewById(R.id.textView11);
-
-        new CountDownTimer(5000,1000){
-            @Override
-            public void onFinish() {
-                // TODO Auto-generated method stub
-                mTextView.setText("0");
-            }
-
-            @Override
-            public void onTick(long millisUntilFinished) {
-                // TODO Auto-generated method stub
-                mTextView.setText(""+millisUntilFinished/1000);
-            }
-
-        }.start();
+        choice1 = (TextView) findViewById(R.id.choice1);
+        choice2 = (TextView) findViewById(R.id.choice2);
+        Arrays.fill(visited, Boolean.TRUE);
+        setup();
+        for(i=0; i<5; i++) {
+            getQuestionAndSetUI();
+        }
     }
-
-    void crossfade(){
-        mLoadingView.animate()
-                .alpha(0f)
-                .setDuration(mLongDurationAnimation)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        mLoadingView.setVisibility(View.GONE);
-                    }
-                });
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -87,20 +81,35 @@ public class SelectQuestion extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    
-    static String[] foodType={
-		"start","正餐","小點心","餓死了","有點飽","下午茶","宵夜"
-		,"甜的","鹹的","米飯","麵食","餃類","關東煮"
-		,"重鹹","清淡","中式","西式","日式","泰式"
-		,"冷的","熱的","平價","高檔","速食","定食"
-		,"肉類","蔬菜","牛肉","豬肉","雞肉","鴨肉","鵝肉","魚肉"
-	};
-	static Boolean visited[]={true};
-	static int RanNum()
+
+    void getQuestionAndSetUI(){
+        int id = TwoChooseOne();
+        choice1.setText(foodType[id]);
+        choice2.setText(foodType[id+1]);
+
+        new CountDownTimer(5000,1000){
+            @Override
+            public void onFinish() {
+                // TODO Auto-generated method stub
+                mTextView.setText("0");
+            }
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // TODO Auto-generated method stub
+                mTextView.setText(""+millisUntilFinished/1000);
+            }
+        }.start();
+    }
+
+
+
+    static int RanNum()
 	{
 		Random rand = new Random();
-		return rand.nextInt(31)+1;
+		return rand.nextInt(30)+1;
 	}
+
 	static void setup()
 	{
 	    Map food=new HashMap();
@@ -108,6 +117,7 @@ public class SelectQuestion extends ActionBarActivity {
 		for(int i=1;i<foodType.length;i++)
 			food.put(i, foodType[i]);
 	}
+
 	public int TwoChooseOne(){
 		int id=0;
 		//random out a number that hasn't visit
@@ -122,4 +132,9 @@ public class SelectQuestion extends ActionBarActivity {
 		}
 	    return id;
 	}
+
+    public void decided(View v){
+        i++;
+        decisions.add((String)((TextView) v).getText());
+    }
 }
